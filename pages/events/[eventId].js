bf6f6d398e7d9e6ff1346d8fb960ef7eb4e9ec9f
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-17 13:08:11 -0500

    Added ErrorAlert component for error messages and EventSummary, EventLogistics, EventContent comps to display event details

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,30 +1,39 @@
-import { useRouter} from 'next/router'
-import { Fragment } from 'react'
-import { getEventById} from '../../dummy-data'
-import EventSummary from '../../components/event-detail/event-summary'
-import EventLogistics from '../../components/event-detail/event-logistics'
-import EventContent from '../../components/event-detail/event-content'
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
+function EventDetailPage(props) {
+  const router = useRouter();
 
-function EventDetailPage(props){
-    const router = useRouter()
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
 
-    const eventId = router.query.eventId
-    const event = getEventById(eventId)
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
 
-    if (!event) {
-        return <p>No event found!</p>
-    }
-
-return (
+  return (
     <Fragment>
-        <EventSummary title={event.title}/>
-        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
-        <EventContent>
-            <p>{event.description}</p>
-        </EventContent>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </Fragment>
-)
+  );
 }
 
-export default EventDetailPage
+export default EventDetailPage;
