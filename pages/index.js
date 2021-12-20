ff99998fec60b222544cb969791662e57bd6c5e0
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-20 11:30:43 -0500

    added revalidate: 1800 to getStaticProps to regenerate Featured Events on Homepage but only at most every 1/2 hour

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,15 @@ export async function getStaticProps() {
     return {
         props: {
             events: featuredEvents
-        }
+        },
+        // every 1/2 hour this page is regenerated for a new incoming request
+        // home page w/featured events probably won't change that often so
+        // 1800 seconds is reasonable revalidate time
+        // we don't want to use getServerSideProps w/ this page b/c it would be
+        // overkill to regenerate the page for every request.
+        // Also if we don't add revalidate property, then we would have to redeploy
+        // the app every time we changed the featured events on the homepage.
+        revalidate: 1800
     }
 }
 
