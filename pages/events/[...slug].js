5cecf65eb44dc5e27894b17cc357e550974662d1
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-20 12:19:12 -0500

    added async fn getServerSideProps to render each filtered events page on the fly for every request
    since there are so many combinations of year/month for the filter, using getStaticProps to pregenerate some or all of the pages doesn't make sense

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,20 +1,76 @@
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import { Fragment } from "react";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
-function FilteredEventsPage() {
-  const router = useRouter();
+function FilteredEventsPage(props) {
+  // const router = useRouter();
 
-  const filterData = router.query.slug;
+  // const filterData = router.query.slug;
 
-  // filterData will return undefined at first render and then it will properly render 2nd time
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
+  // // filterData will return undefined at first render and then it will properly render 2nd time
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>;
+  // }
+
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
+
+  // // converts strings to numbers
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
+
+  if (props.hasError) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values.</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
+
+  const filteredEvents = props.events
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>There are no events that meet your search criteria.</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(props.date.year, props.date.month - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+// since we have numerous year/month combinations, only pregenerating
+// some of the pages would be difficult to choose; also pregenerating all
+// of the pages would be too much. Therefore, rendering the page on the fly
+// with getServerSideProps is probably the best option
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
 
   const filteredYear = filterData[0];
   const filteredMonth = filterData[1];
@@ -31,46 +87,32 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <Fragment>
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values.</p>
-        </ErrorAlert>
-
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </Fragment>
-    );
+    // can't return JSX is getServerSideProps; instead could return the 404 page if erroneous values
+    return {
+      // using hasError prop b/c we have error JSX already in the component above
+      props: { hasError: true },
+      // notFound: true
+      // we could also redirect to an error page if we had one
+      //redirect: {
+      // destination: '/error'
+      // }
+    };
   }
 
-  const filteredEvents = getFilteredEvents({
+  const filteredEvents = await getFilteredEvents({
     year: numYear,
-    month: numMonth,
-  });
+    month: numMonth
+  })
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <Fragment>
-        <ErrorAlert>
-          <p>There are no events that meet your search criteria.</p>
-        </ErrorAlert>
-
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </Fragment>
-    );
-  }
-
-  const date = new Date(numYear, numMonth - 1);
-
-  return (
-    <Fragment>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </Fragment>
-  );
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth
+      }
+    },
+  };
 }
 
 export default FilteredEventsPage;
