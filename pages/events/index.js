4f171aba6142d2b701d75dc84cf49555a65d5c46
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-20 12:19:50 -0500

    added async fn getStaticProps for All Events page w/revalidate set at 60s

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,13 @@
-import {getAllEvents} from '../../dummy-data'
+import {getAllEvents} from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
 import {Fragment} from 'react'
 import {useRouter} from 'next/router'
 
-function AllEventsPage(){
-const events = getAllEvents()
+function AllEventsPage(props){
+// const events = getAllEvents()
 const router = useRouter()
+const {events} = props
 
 function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`
@@ -18,6 +19,17 @@ function findEventsHandler(year, month) {
         <EventsSearch onSearch={findEventsHandler}/>
         <EventList items={events}/>
     </Fragment>
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents()
+
+    return {
+        props: {
+            events: events
+        },
+        revalidate: 60
+    }
 }
 
 export default AllEventsPage
