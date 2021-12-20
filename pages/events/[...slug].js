6751ab88eb3b9682305f96e5ce2da65ed62d4e19
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-20 14:55:25 -0500

    changed data fetching from server side to client side using SWR package

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,29 +1,63 @@
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+
 import { getFilteredEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
-import { Fragment } from "react";
+
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage(props) {
-  // const router = useRouter();
+  const [loadedEvents, setLoadedEvents] = useState();
+  const router = useRouter();
 
-  // const filterData = router.query.slug;
+  const filterData = router.query.slug;
 
-  // // filterData will return undefined at first render and then it will properly render 2nd time
-  // if (!filterData) {
-  //   return <p className="center">Loading...</p>;
-  // }
+  const { data, error } = useSWR(
+    "https://next-events-d4733-default-rtdb.firebaseio.com/events.json", (url) => fetch(url).then(res => res.json())
+  );
 
-  // const filteredYear = filterData[0];
-  // const filteredMonth = filterData[1];
+  console.log(data);
 
-  // // converts strings to numbers
-  // const numYear = +filteredYear;
-  // const numMonth = +filteredMonth;
+  useEffect(() => {
+    if (data) {
+      const events = [];
 
-  if (props.hasError) {
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  console.log(loadedEvents);
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // converts strings to numbers
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -37,7 +71,13 @@ function FilteredEventsPage(props) {
     );
   }
 
-  const filteredEvents = props.events
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -53,7 +93,7 @@ function FilteredEventsPage(props) {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <Fragment>
@@ -67,52 +107,52 @@ function FilteredEventsPage(props) {
 // some of the pages would be difficult to choose; also pregenerating all
 // of the pages would be too much. Therefore, rendering the page on the fly
 // with getServerSideProps is probably the best option
-export async function getServerSideProps(context) {
-  const { params } = context;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
 
-  const filterData = params.slug;
+//   const filterData = params.slug;
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
 
-  // converts strings to numbers
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+//   // converts strings to numbers
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
-    // can't return JSX is getServerSideProps; instead could return the 404 page if erroneous values
-    return {
-      // using hasError prop b/c we have error JSX already in the component above
-      props: { hasError: true },
-      // notFound: true
-      // we could also redirect to an error page if we had one
-      //redirect: {
-      // destination: '/error'
-      // }
-    };
-  }
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     // can't return JSX is getServerSideProps; instead could return the 404 page if erroneous values
+//     return {
+//       // using hasError prop b/c we have error JSX already in the component above
+//       props: { hasError: true },
+//       // notFound: true
+//       // we could also redirect to an error page if we had one
+//       //redirect: {
+//       // destination: '/error'
+//       // }
+//     };
+//   }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth
-  })
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth
+//   })
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth
-      }
-    },
-  };
-}
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth
+//       }
+//     },
+//   };
+// }
 
 export default FilteredEventsPage;
