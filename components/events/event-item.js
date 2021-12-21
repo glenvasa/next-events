df3f32f56e6cd283fa888c21c6aba53487640a47
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 12:17:06 -0500

    replaced img tag with Next Image component to optimize images and lazy load by default

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
@@ -18,7 +20,10 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      {/* Next Image will create multiple versions of the image based on browser/operating system and cache them for later similar requests */}
+      {/* Images are by default lazy loaded in Next; so if it doesn't initially appear in the browser and then you scroll down or increase browser size, a new request forr the "hidden" image will automatically appear in Network tab and the image will be loaded */}
+      <Image src={"/" + image} alt={title} width={250} height={160}/>
+     
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
