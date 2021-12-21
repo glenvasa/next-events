0f9d053c3b348924b2945c734eb7b36849bec6f4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 11:38:23 -0500

    applied title and meta description tags in _app.js Head component which applies to all pages unless overwritten with same tags in specific pages
    Since specific pages load after _app.js file, the latest Head tags will be injected into head element for specific pages.

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ function MyApp({ Component, pageProps }) {
   return <Layout>
     {/* use to inject meta data into head element across all pages */}
     <Head>
+      {/* title and meta description below would apply to all pages unless overwritten with a title tag in a specific page */}
+      <title>Next Events</title>
+      <meta name='description' content='NextJS Events' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
      <Component {...pageProps} />
