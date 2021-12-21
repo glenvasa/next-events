4dbf6e9484e4131285a77a37775b3ddc00023f70
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 10:36:12 -0500

    added Head component title/meta data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import {getAllEvents} from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
@@ -16,6 +17,10 @@ function findEventsHandler(year, month) {
 }
 
     return <Fragment>
+        <Head>
+            <title>All Events</title>
+            <meta name="description" content="Find a lot of great events that allow you to evolve..."/>
+        </Head>
         <EventsSearch onSearch={findEventsHandler}/>
         <EventList items={events}/>
     </Fragment>
