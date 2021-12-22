8904ecfebbd702bced48d37d39c8e52c7aba59e3
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-22 10:03:54 -0500

    created api/comments/[eventId] POST and GET routes to post a comment to a specific event or get all comments for a specific event

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,37 @@
+function handler(req, res){
+    const eventId = req.query.eventId
+    
+    if (req.method === 'POST') {
+        // server-side validation
+      const {email, name, text} = req.body
+
+      if (!email.includes('@') || !name || name.trim() === '' || !text || text.trim() === ''){
+        res.status(422).json({message: 'Invalid input'})  
+        return
+      }
+
+  
+      const newComment = {
+          id: new Date().toISOString(),
+          email, 
+          name, 
+          text
+      }   
+       
+      console.log(newComment);
+      res.status(201).json({message: 'New comment created', comment: newComment})
+    }
+
+    if (req.method === 'GET') {
+        const dummyList = [
+            { id: 'c1', name: 'Glen', text: 'A first comment'},
+            { id: 'c2', name: 'Ethan', text: 'A second comment'}
+        ]
+
+        res.status(200).json({comments: dummyList})
+    }
+
+    
+}
+
+export default handler
