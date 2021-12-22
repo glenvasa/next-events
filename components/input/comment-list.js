0994af3622c1f18e550a9a44f72e04ea3e226e0f
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-22 10:45:23 -0500

    mapping all comments for an event  into an li displaying text and name

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,10 +1,17 @@
 import classes from './comment-list.module.css';
 
-function CommentList() {
+function CommentList(props) {
+  console.log(props.items);
   return (
     <ul className={classes.comments}>
-      {/* Render list of comments - fetched from API */}
-      <li>
+     
+      {props.items.map(item => 
+        <li key={item.id}>
+          <p>{item.text}</p>
+          <div>By <address>{item.name}</address></div>
+        </li>
+      )}
+      {/* <li>
         <p>My comment is amazing!</p>
         <div>
           By <address>Maximilian</address>
@@ -15,7 +22,7 @@ function CommentList() {
         <div>
           By <address>Maximilian</address>
         </div>
-      </li>
+      </li> */}
     </ul>
   );
 }
