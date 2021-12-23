66ad1f5ab1a7a5252a4db73c742e1b3bd629e146
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 11:06:00 -0500

    wrapping entire application in NotificationContextProvider

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import Head from 'next/head'
 
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
-import Notification from '../components/ui/notification'
+
+import { NotificationContextProvider } from '../store/notification-context'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
+  return (
+  <NotificationContextProvider>
+
+    <Layout>
     {/* use to inject meta data into head element across all pages */}
     <Head>
       {/* title and meta description below would apply to all pages unless overwritten with a title tag in a specific page */}
@@ -14,9 +18,11 @@ function MyApp({ Component, pageProps }) {
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
      <Component {...pageProps} />
-     {/* <Notification title='Test' message='This is a test.' status='error'/> */}
+     
   </Layout>
- 
+  </NotificationContextProvider>
+  
+  )
 }
 
 export default MyApp
