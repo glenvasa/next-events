861cda6500596b938e5f1464320c424d597d2534
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 11:40:20 -0500

    using context to display pending, success, and error messages when new comment submit clicked. Also included Loading... message when all comments for an event are loading

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,43 +1,83 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     if (showComments) {
-      fetch(`/api/comments/${eventId}`).then(response => response.json()).then(data => setComments(data.comments))
+      setIsFetchingComments(true);
+      fetch(`/api/comments/${eventId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setComments(data.comments);
+          setIsFetchingComments(false);
+        });
     }
-  }, [showComments])
+  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
-    // send data to API
+    notificationCtx.showNotification({
+      title: "Sending comment...",
+      message: "Your comment is currently being stored into the database.",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(commentData),
       headers: {
-        'Content-Type' : 'application/json'
-      }
-    }).then(response => response.json()).then(data => console.log(data))
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong");
+        });
+      })
+
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success",
+          message: "Successfully added your comment.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong.",
+          status: "error",
+        });
+      });
   }
 
   return (
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
+        {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments}/>}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }
