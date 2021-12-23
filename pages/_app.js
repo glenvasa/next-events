ac563207a363c32ccf79245172f783941fe9ad73
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 11:38:59 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import '../styles/globals.css'
-import Layout from '../components/layout/layout'
+import "../styles/globals.css";
+import Layout from "../components/layout/layout";
 
-import { NotificationContextProvider } from '../store/notification-context'
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <NotificationContextProvider>
-
-    <Layout>
-    {/* use to inject meta data into head element across all pages */}
-    <Head>
-      {/* title and meta description below would apply to all pages unless overwritten with a title tag in a specific page */}
-      <title>Next Events</title>
-      <meta name='description' content='NextJS Events' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-     <Component {...pageProps} />
-     
-  </Layout>
-  </NotificationContextProvider>
-  
-  )
+    <NotificationContextProvider>
+      <Layout>
+        {/* use to inject meta data into head element across all pages */}
+        <Head>
+          {/* title and meta description below would apply to all pages unless overwritten with a title tag in a specific page */}
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
