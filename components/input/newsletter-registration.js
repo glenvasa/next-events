694f80e9d7058d7116eecb7bcd2c401ae719d518
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 11:04:38 -0500

    imported NotificationContext to showNotification when signing up for newsletter
    Pending, Success, and Error notifications will display depending on the status of the http request when email submitted

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,20 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 import classes from "./newsletter-registration.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext)
 
   function registrationHandler(event) {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending'
+    })
 
     fetch("/api/newsletter", {
       method: "POST",
@@ -14,7 +22,30 @@ function NewsletterRegistration() {
       headers: {
         "Content-Type": "application/json",
       },
-    }).then(response => response.json()).then(data => console.log(data))
+    }).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+   // we need to throw a new Error here if there is an error b/c 400 or 500 errors by themselves
+   // won't get us into the catch bloc to show the Error notification. This code will fix that.
+      return response.json().then(data => {
+        throw new Error(data.message || 'Something went wrong')
+      })
+    })
+    .then(data => {
+      notificationCtx.showNotification({
+        title: 'Success!',
+        message: 'Successfully registered for newsletter.',
+        status: 'success'
+      })
+    })
+    .catch(error => {
+      notificationCtx.showNotification({
+        title: 'Error!',
+        message: error.message || 'Something went wrong.',
+        status: 'error'
+      })
+    })
   }
 
   return (
