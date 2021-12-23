ce1c3f2cadc863923ee8977f7272d3e19b4c9cfd
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 09:23:24 -0500

    added error handling w/try-catch blocks

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,12 +1,21 @@
-import { MongoClient } from "mongodb";
-import {getAllDocuments} from '../../../helpers/db-util'
+
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from "../../../helpers/db-util";
 
 async function handler(req, res) {
   const eventId = req.query.eventId;
 
-  const client = await MongoClient.connect(
-    "mongodb+srv://next-events:next-events@cluster0.oturw.mongodb.net/events?retryWrites=true&w=majority"
-  );
+  let client;
+
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Failed to connect to database" });
+    return;
+  }
 
   if (req.method === "POST") {
     // server-side validation
@@ -20,6 +29,7 @@ async function handler(req, res) {
       text.trim() === ""
     ) {
       res.status(422).json({ message: "Invalid input" });
+      client.close()
       return;
     }
 
@@ -30,40 +40,51 @@ async function handler(req, res) {
       eventId,
     };
 
-    const db = client.db();
+    let result;
 
-    const result = await db.collection("comments").insertOne(newComment);
+    try {
+      result = await insertDocument(client, "comments", newComment);
+      res
+      .status(201)
+      .json({ message: "New comment created", comment: newComment });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Failed to insert data into database collection" });
+    }
 
     console.log(result);
 
     // this code would add the MongoDb auto created id to the newComment object we send back
     // front end with 201 code below.
     // However, it looks like MongoDb automatically added it to the result object it returned so I will leave out this code for now
-    //   newComment.id = result.insertedId
+    //   newComment._id = result.insertedId
 
-    res
-      .status(201)
-      .json({ message: "New comment created", comment: newComment });
+    
   }
 
   if (req.method === "GET") {
-    const db = client.db();
-
     // will get us all documents in comments collection and covert to array
     // which is the format we need to map them in comments-list component
     // sort will sort mongodb's created _id in decending order so that newest comment returned first
     // const documents = await db.collection("comments").find().sort({ _id: -1}).toArray();
 
     // changing how we call getAllDocuments to only get comments for a specific event
-    // importing getAllDocuments from helper db-util.js 
-    const documents = await getAllDocuments(
-        client,
-        'comments',
-        { _id: -1},
-        {eventId: eventId}
-    )
+    // importing getAllDocuments from helper db-util.js
 
-    res.status(200).json( {comments: documents});
+    let documents;
+
+    try {
+      documents = await getAllDocuments(
+        client,
+        "comments",
+        { _id: -1 },
+        { eventId: eventId }
+      );
+      res.status(200).json({ comments: documents });
+    } catch (error) {
+      res.status().json({ message: "Getting comments failed" });
+    }
   }
 
   client.close();
