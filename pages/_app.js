7093beb6bdd23b4d7f2c8af8aec0b577debaf080
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 09:41:25 -0500

    testing Notification component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
+import Notification from '../components/ui/notification'
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }) {
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
      <Component {...pageProps} />
+     {/* <Notification title='Test' message='This is a test.' status='error'/> */}
   </Layout>
  
 }
